0f777a8ccf5a04ab9e89fe2f6811393a248205fa
Yu Shi <yushiys95@gmail.com>
2021-04-08 19:02:56 -0400

    commit tree 2807f63de94bc4454d9a228029af9be004855e8f

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form(props) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input
+        type="text"
+        onChange={props.handleFirstNameChange}
+        value={props.firstName}
+        placeholder="Enter first name..."
+      />
+      <input
+        type="text"
+        onChange={props.handleLastNameChange}
+        value={props.lastName} 
+        placeholder="Enter last name..."
+      />
       <button type="submit">Submit</button>
     </form>
   );
