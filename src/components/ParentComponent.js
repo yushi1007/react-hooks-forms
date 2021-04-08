16ebc9779da1bc6b9dea502154d7bd24b43d6834
Yu Shi <yushiys95@gmail.com>
2021-04-08 19:03:01 -0400

    Done.

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,33 @@
+// src/components/ParentComponent
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from './DisplayData';
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(event) {
+    event.preventDefault();
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    event.preventDefault();
+    setLastName(event.target.value);
+  }
+
+  return (
+    <div>
+    <Form
+      firstName={firstName}
+      lastName={lastName}
+      handleFirstNameChange={handleFirstNameChange}
+      handleLastNameChange={handleLastNameChange}
+    />
+    <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
